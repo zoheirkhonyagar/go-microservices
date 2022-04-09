459c7d8c1eff91627f59c21d521020994bbad163
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2022-04-09 23:17:00 +0430

    added first web web-server

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
 		log.Println("hello world")
+
+		data, error := ioutil.ReadAll(request.Body)
+
+		if error != nil {
+			http.Error(responseWriter, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Data %s", data)
+
+		fmt.Fprintf(responseWriter, "Hello %s", data)
 	})
 
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
